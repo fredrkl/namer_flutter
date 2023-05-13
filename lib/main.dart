ae00c567f0c480c98b8dfdbb9ae5ca61f5b9ad26
Fredrik Klingenberg <fredrkl@gmail.com>
2023-05-13 09:48:41 +0200

    feat: generate word pairs
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -39,10 +44,9 @@ class MyHomePage extends StatelessWidget {
         children: [
           Text('A random idea:'),
           Text(appState.current.asLowerCase),
-          // ↓ Add this.
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNext();
             },
             child: Text('Next'),
           ),
